db17839d37949d3e1ca2c68b53c8bba9a5bdd9c7
discordbotit <71970751+discordbotit@users.noreply.github.com>
2020-10-03 21:14:23 +0300

    Create medium_questions.js
    dataschema medium kysymyksille

---
+++ models/medium_questions.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+
+//Dokumentin rakenteen määrittely:
+const uusiSchema = mongoose.Schema({
+    category: String,
+    type: String,
+    difficulty: String,
+    question: String,
+    correct_answer: String,
+    incorrect_answers: Array,
+})
+
+//Malli luo tietokantaan "medium_questionData(s)" kokoelman
+module.exports = mongoose.model("medium_questionData", uusiSchema);
