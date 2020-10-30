b4250f1e62ddd72057e1f05995088ed4b77ccfaa
Yolanda Theodorakis <theodorakisyolanda@gmail.com>
2020-10-30 15:29:14 +0200

    Checks if role exists on server

--- commands/player/setRole.js
+++ commands/player/setRole.js
@@ -11,12 +11,16 @@ const Data = require("../../models/players.js"); //Viitataan malliin users.js
 
 module.exports.run = async (bot, message, args) => {
 
-    //Tarkistaa että onko pelaajalla jo pelirooli olemassa
-    if(message.member.roles.cache.find(r => r.name === 'Haluatko miljonääriksi')) {
-        return(message.reply("Sinulla on jo tämä rooli!"))
+    //Tarkistaa, onko rooli olemassa serverillä
+    if(message.guild.roles.cache.find(r => r.name !== "Haluatko miljonääriksi")) {
+        return(message.reply("Valitettavasti tällä serverillä ei ole kyseistä roolia."))
+    }
+    //Tarkistaa, onko pelaajalla jo pelirooli olemassa
+    if(message.member.roles.cache.find(r => r.name === "Haluatko miljonääriksi")) {
+        return(message.reply("Sinulla on jo tämä rooli."))
     }
     //Etsii roolin serveriltä
-    let role = message.guild.roles.cache.find(r => r.name === 'Haluatko miljonääriksi')
+    let role = message.guild.roles.cache.find(r => r.name === "Haluatko miljonääriksi");
     //Asetetaan komennon kirjoittajalle tämä rooli
     let user = message.guild.members.cache.get(message.author.id);
     user.roles.add(role);
@@ -27,10 +31,10 @@ module.exports.run = async (bot, message, args) => {
         peli_kaynnissa: false,
         kysymys_kytkin: false,
         voitot: 0,
-        kayttamattomat_oljenkorret: ['50-50','kilauta kaverille','kysy katsomolta']
+        kayttamattomat_oljenkorret: ["50-50","kilauta kaverille","kysy katsomolta"]
         })
         newData.save().catch(err => console.log(err)); //Dokumentin tallennus tietokantaan
-        message.reply("Sinulla on nyt rooli ja voit aloittaa pelaamisen!")
+        message.reply("Sinulla on nyt rooli ja voit aloittaa pelaamisen!");
     }
 
     
