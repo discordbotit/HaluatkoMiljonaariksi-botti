a022a02c7d5080db4b20545b98a1031f3895b8d5
Yolanda Theodorakis <theodorakisyolanda@gmail.com>
2021-01-26 18:53:36 +0200

    Update addRole.js

--- commands/admin/addRole.js
+++ commands/admin/addRole.js
@@ -1,28 +1,27 @@
 module.exports.run = async (bot, message, args) => {
 
-
     let roleName = 'Haluatko miljonääriksi';
-    //Suoritetaan haku, löytyykö roolin nimi jo serveriltä
+
+    // Suoritetaan haku, löytyykö roolin nimi jo serveriltä
     let role = message.guild.roles.cache.find(x => x.name === roleName);
+
     if (!role) {
     // Jos ei löydy, niin luodaan sellainen
         message.guild.roles.create({
             data: {
-            name: 'Haluatko miljonääriksi',
-            color: 'BLUE',
+                name: 'Haluatko miljonääriksi',
+                color: 'BLUE',
             },
             reason: '',
         })
-            .then(console.log("Lisättiin Haluatko miljonääriksi rooli serverille!"))
+            .then(console.log("Haluatko miljonääriksi -rooli lisätiin serverille."))
             .catch(console.error);
     } else {
         return(console.log("Haluatko miljonääriksi rooli löytyy jo serveriltä!"));
-    }  
-    
+    }   
 }
 
-
-//komento toimii näillä sanoilla   
+// Komento toimii näillä sanoilla   
 module.exports.help = {
     name: "add_role",
     aliases: []
