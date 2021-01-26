81038adbdbdb86d5fac7a30306aa8a7468a453f5
Yolanda Theodorakis <theodorakisyolanda@gmail.com>
2021-01-26 18:54:50 +0200

    Update keskeytaPeli.js

--- commands/player/keskeytaPeli.js
+++ commands/player/keskeytaPeli.js
@@ -1,16 +1,15 @@
-const mongoose = require("mongoose"); //Mongoose moduuli mukaan
-const botconfig = require("../../botconfig.json"); //Määritellään botin asetukset JSON-filusta
+const mongoose = require("mongoose"); // Mongoose moduuli mukaan
+const botconfig = require("../../botconfig.json"); // Määritellään botin asetukset JSON-tiedostosta
 
-//Yhdistetään MongoDB:hen:
+// Yhdistetään MongoDB:hen
 mongoose.connect(botconfig.mongoPass, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
 
-const Data = require("../../models/players.js"); //Viitataan malliin users.js
+const Data = require("../../models/players.js"); // Viitataan malliin users.js
 
-
-//Komennolla käyttäjä voi lopettaa olemassa olevan pelin, tämän myötä nollataan voitot ja muutetaan oleelliset kytkimet falseksi
+// Komennolla käyttäjä voi lopettaa olemassa olevan pelin, tämän myötä nollataan voitot ja muutetaan oleelliset kytkimet falseksi
 module.exports.run = async (bot, message, args) => {
 
     Data.findOne ({
@@ -19,33 +18,33 @@ module.exports.run = async (bot, message, args) => {
 
     }, (err, data) => {
 
-        //Jossei ole peliä käynnissä, tästä mainitaan
+        // Jos peliä ei ole käynnissä, tästä mainitaan
         if (data.peli_kaynnissa === false) {
-            return message.reply("Sinulla ei ole käynnissä olevaa peliä").catch(err => console.log(err));
-        } else {
-            
+            return message.reply("Sinulla ei ole käynnissä olevaa peliä.").catch(err => console.log(err));
+        } 
+        else {
             Data.findOneAndUpdate({pelaajan_id: message.author.id}, {peli_kaynnissa : false}, (err, data) => {
-                if(err){
+                if (err) {
                     console.log(err)
                 } 
             })
             
             Data.findOneAndUpdate({pelaajan_id: message.author.id}, {kysymys_kytkin : false}, (err, data) => {
-                if(err){
+                if (err) {
                     console.log(err)
                 } 
             })
         
             Data.findOneAndUpdate({pelaajan_id: message.author.id}, {voitot : 0}, (err, data) => {
-                if(err){
+                if (err) {
                     console.log(err)
                 } 
             })
         
-            //Jos 50-50 oljenkorsi käytettiin pelissä, lisätään se resetoinnin yhteydessä käytössä olevien oljenkorsien listalle takaisin
+            // Jos 50-50 oljenkorsi käytettiin pelissä, lisätään se resetoinnin yhteydessä käytössä olevien oljenkorsien listalle takaisin
             if (!data.kayttamattomat_oljenkorret.includes('50-50')) {
-                Data.findOneAndUpdate({pelaajan_id: message.author.id}, { $push: { kayttamattomat_oljenkorret: [ "50-50" ] } }, (err, data) => {
-                    if(err){
+                Data.findOneAndUpdate({pelaajan_id: message.author.id}, {$push: {kayttamattomat_oljenkorret: [ "50-50" ]}}, (err, data) => {
+                    if (err) {
                         console.log(err)
                     } 
                 })
@@ -57,8 +56,7 @@ module.exports.run = async (bot, message, args) => {
     
 }
 
-
-//komento toimii näillä sanoilla   
+// Komento toimii näillä sanoilla   
 module.exports.help = {
 name: "lopeta",
 aliases: []
